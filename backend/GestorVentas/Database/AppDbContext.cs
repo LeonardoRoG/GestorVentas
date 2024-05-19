7c6b0ab83f2553e913eed8c993a14916fca0d0ca
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-05-19 16:35:59 -0300

    Added DbContext

---
+++ backend/GestorVentas/Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using GestorVentas.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorVentas.Database;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Proveedor> Proveedores { get; set; }
+
+    public DbSet<Categoria> Categorias { get; set; }
+
+    public DbSet<Producto> Productos { get; set; }
+
+    public DbSet<Vehiculo> Vehiculos { get; set; }
+
+    public DbSet<Empleado> Empleados { get; set; }
+
+    public DbSet<Cliente> Clientes { get; set; }
+
+    public DbSet<Orden> Ordenes { get; set; }
+
+    public DbSet<DetalleOrden> DetalleOrdenes { get; set; }
+}
--- backend/GestorVentas/Program.cs
+++ backend/GestorVentas/Program.cs
@@ -1,3 +1,7 @@
+using Carter;
+using GestorVentas.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,40 +9,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var config = builder.Configuration;
+
+builder.Services.AddCarter();
+
+// Add CORS configuration
+builder.Services.AddCors(options => options.AddPolicy("GestorVentas2024",
+    policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(config.GetConnectionString("AppDb")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(option => option.EnableTryItOutByDefault());
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Using CORS
+app.UseCors("GestorVentas2024");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapCarter();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
