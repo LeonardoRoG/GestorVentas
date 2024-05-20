3fd202d3297549dd76a938c64696c51e230fa335
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-05-20 17:59:39 -0300

    Categorias endpoints modified

--- backend/GestorVentas/Endpoints/CategoriaEndpoints.cs
+++ backend/GestorVentas/Endpoints/CategoriaEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using Carter;
+using GestorVentas.Endpoints.DTO;
 using GestorVentas.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestorVentas.Endpoints;
 
@@ -13,6 +15,34 @@ public class CategoriaEndpoints : ICarterModule
         {
             var categorias = categoriaService.GetAllCategorias();
             return Results.Ok(categorias);
+
+        }).WithTags("Categorias");
+
+        app.MapGet("/{idCategoria:int}", (ICategoriaService categoriaService, int idCategoria) =>
+        {
+            var categoria = categoriaService.GetCategoria(idCategoria);
+            return Results.Ok(categoria);
+
+        }).WithTags("Categorias");
+
+        app.MapPost("/", ([FromServices]ICategoriaService categoriaService, [FromBody]CategoriaRequestDto categoriaDto) =>
+        {
+            categoriaService.AddCategoria(categoriaDto);
+            return Results.Created();
+
+        }).WithTags("Categorias");
+
+        app.MapDelete("/{idCategoria:int}", (ICategoriaService categoriaService, int idCategoria) =>
+        {
+            categoriaService.RemoveCategoria(idCategoria);
+            return Results.NoContent();
+
+        }).WithTags("Categorias");
+
+        app.MapPut("/", ([FromServices]ICategoriaService categoriaService, int idCategoria, [FromBody]CategoriaRequestDto categoriaDto) =>
+        {
+            categoriaService.UpdateCategoria(idCategoria, categoriaDto);
+            return Results.Ok();
 
         }).WithTags("Categorias");
     }
